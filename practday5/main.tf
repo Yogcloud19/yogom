2aae86369f3a301e0c3a8d0668ff61f593d7655f
Yogcloud19 <yograthod19@gmail.com>
2025-07-11 00:12:31 +0530

    message

--- practday5/main.tf
+++ practday5/main.tf
@@ -10,11 +10,5 @@ resource "aws_instance" "day4" {
   }
 }
 
-resource "aws_instance" "demo05" {
-  ami           = "ami-05ffe3c48a9991133"
-  instance_type = "t2.nano"
-  tags = {
-    Name = "demo-05"
-  }
-}
+
 
