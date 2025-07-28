b3efa2edc817da44f1f61405f0bc44194b59ef82
Yogcloud19 <yograthod19@gmail.com>
2025-07-28 23:02:45 +0530

    Update main.tf

--- prctday6/main.tf
+++ prctday6/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "name" {
 resource "aws_subnet" "name" {
     vpc_id = aws_vpc.name.id
     cidr_block = "10.0.0.0/24"
-    availability_zone = "us-east-1a"
+    availability_zone = "ap-south-1a"
     tags = {
       Name = "test-subnet"
     }
